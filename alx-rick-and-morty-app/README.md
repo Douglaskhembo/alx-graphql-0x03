1bcf3a4a28730e63befcdf04fc05d26fe0a9e338
khembo <khembo.douglas.juma@gmail.com>
2025-07-02 23:25:26 +0300

    Create the ErrorBoundary Component

---
+++ alx-rick-and-morty-app/README.md
@@ -0,0 +1,16 @@
+# Rick and Morty GraphQL App
+
+This project displays paginated Rick and Morty episodes using Apollo Client, TypeScript, and GraphQL.
+
+## Features
+
+- Apollo Client integration
+- TypeScript interfaces for episodes
+- Pagination
+- Styled with Tailwind CSS
+
+## Usage
+
+```bash
+npm install
+npm run dev
---
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -0,0 +1,18 @@
+// pages/_app.tsx
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import client from '../graphql/apolloClient'
+import ErrorBoundary from '../components/ErrorBoundary'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </ApolloProvider>
+  )
+}
+
+export default MyApp
